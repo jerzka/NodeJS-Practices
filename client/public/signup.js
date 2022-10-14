7075a5881699dfcd9ce649cfe2bfb93eb53bcad1
jerzka <kjerzmanowska@gmail.com>
2022-10-14 17:28:02 -0500

    Update signup.js

--- client/public/signup.js
+++ client/public/signup.js
@@ -1,19 +1,42 @@
-// const emailInput = document.getElementById();
-(() => {
-    'use strict'
+// // const emailInput = document.getElementById();
+// (() => {
+//     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+//     // Loop over them and prevent submission
+//     Array.from(forms).forEach(form => {
+//       form.addEventListener('submit', event => {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
   
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+//   })()
+const url ="http://localhost:3002/api/users/register";
+
+const userInput = document.getElementById("userInput");
+const emailInput = document.getElementById("emailInput");
+const passwordInput = document.getElementById("paswordInput");
+
+const submitBtn = document.getElementById("submit-btn");
+
+const handleSignup = async () => {
+  const user = JSON.stringify({"email":emailInput, "password":passwordInput});
+  fetch(url, { // `${url}/${id}`
+    method: "POST",
+    body: user,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then(response => response.json()
+  .then(data=>{
+    window.location('signin.html')
+  })
+  )};
+
+submitBtn.addEventListener("submit", handleSignup);
